9b224bce748666b13581ba9f6a209af827a5fd40
unknown <jgarverick@3rdl-jgarverick.3rdl.com>
2013-03-27 23:10:30 -0400

    Adding durandal widget implementation (doc to follow)

---
+++ durandal/widgets/pagedGrid/controller.js
@@ -0,0 +1,24 @@
+﻿define(function (require) {
+    var widget = require('durandal/widget');
+    var pagedGrid = require('./breezePager');
+
+    var ctor = function (element, settings) {
+        this.settings = settings;
+        settings.grid = settings.grid || new breezePagedGrid();
+        settings.grid.Init();
+        $(settings.dataOptions.columns).each(function (idx, item) {
+            settings.grid.AddColumn(item.header, item.field, item.display, item.key, item.link);
+        });
+
+        settings.grid.DataPromise = function () {
+            var query = settings.dataOptions.query.inlineCount(true).skip(settings.grid.Pager().skipItems()).take(parseInt(settings.grid.Pager().itemsPerPage()));
+            return settings.dataOptions.manager.executeQuery(query);
+        };
+
+        settings.grid.GetData();
+    };
+
+    return ctor;
+
+
+});
