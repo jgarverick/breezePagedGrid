8f8f8edb68486b3dc8f514dcfef321b755003f4e
unknown <jgarverick@3rdl-jgarverick.3rdl.com>
2013-03-28 09:00:54 -0400

    Updated settings and added some error stuff

--- durandal/widgets/pagedGrid/controller.js
+++ durandal/widgets/pagedGrid/controller.js
@@ -11,8 +11,29 @@
         });
 
         settings.grid.DataPromise = function () {
-            var query = settings.dataOptions.query.inlineCount(true).skip(settings.grid.Pager().skipItems()).take(parseInt(settings.grid.Pager().itemsPerPage()));
-            return settings.dataOptions.manager.executeQuery(query);
+            var mgr = null;
+            // Figure out if the manager was passed in, or if the controller name is passed.  If neither, alert.
+            if (settings.dataOptions.manager) {
+                mgr = settings.dataOptions.manager;
+            } else if (settings.dataOptions.controller && settings.dataOptions.controller.length > 0) {
+                mgr = new breeze.EntityManager(settings.dataOptions.controller);
+            } else {
+                alert("You must pass in either the path to the API controller OR the EntityManager object.");
+                return;
+            }
+            // Figure out if the query was passed in, or if the entity set's name is passed.  If neither, alert.
+            var query = null;
+            if (settings.dataOptions.query) {
+                query = settings.dataOptions.query.inlineCount(true);
+            } else if (settings.dataOptions.entitySet && settings.dataOptions.entitySet.length > 0) {
+                query = new breeze.EntityQuery().from(settings.dataOptions.entitySet);
+            } else {
+                alert("You must pass in either the name of the EntitySet to query OR the EntityQuery object.");
+                return;
+            }
+            // Finalize
+            query = query.skip(settings.grid.Pager().skipItems()).take(parseInt(settings.grid.Pager().itemsPerPage()));
+            return mgr.executeQuery(query);
         };
 
         settings.grid.GetData();
